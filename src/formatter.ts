5da38eff9f398ffe5ce5afd0b9548cbac9b19341
Lane Henslee <huncho@hygo.ai>
2024-11-11 05:13:27 -0800

    format works a little

---
+++ src/formatter.ts
@@ -0,0 +1,91 @@
+import * as vscode from "vscode";
+import { getCommentDelimiter } from "./utils";
+
+class Line {
+  text: string;
+  range: vscode.Range;
+  constructor(text: string, range: vscode.Range) {
+    this.text = text;
+    this.range = range;
+  }
+
+  format(largest_length: number, delimiter: string) {
+    const textSplit = this.text.split(delimiter);
+    if (textSplit.length === 1) {
+      console.log(`No delimiter found in ${this.text}. Skipping`);
+      return this.text;
+    }
+    if (textSplit[0].trim() === "") {
+      console.log(`No text before ${delimiter}. Skipping ${this.text}`);
+      return this.text;
+    }
+    const spaces = " ".repeat(
+      largest_length - this.text.split(delimiter)[0].length
+    );
+    console.log(`Adding ${spaces} to ${this.text}`);
+    return textSplit[0] + spaces + delimiter + textSplit[1];
+  }
+
+  updateEdit(
+    edit: vscode.TextEditorEdit,
+    largest_length: number,
+    delimiter: string
+  ) {
+    edit.replace(this.range, this.format(largest_length, delimiter));
+  }
+}
+
+class Block {
+  largest_length = 0;
+  lines: Line[] = [];
+
+  addLine(text: string, range: vscode.Range, delimiter: string) {
+    const length = text.split(delimiter)[0].length;
+    if (length > this.largest_length) {
+      this.largest_length = length;
+    }
+    this.lines.push(new Line(text, range));
+  }
+
+  updateEdit(edit: vscode.TextEditorEdit, delimiter: string) {
+    this.lines.forEach((line) => {
+      line.updateEdit(edit, this.largest_length, delimiter);
+    });
+  }
+}
+
+class Formatter {
+  blocks: Block[] = [];
+
+  run(editor: vscode.TextEditor) {
+    const doc = editor.document;
+    const text = doc.getText();
+    const lines = text.split("\n");
+    console.log(`Formatting ${lines.length} lines`);
+    let block: Block | null = null;
+    const commentDelimiter = getCommentDelimiter(editor);
+
+    lines.forEach((lineText, index) => {
+      const line = doc.lineAt(index);
+      if (line.text.includes(commentDelimiter)) {
+        if (!block) {
+          block = new Block();
+          this.blocks.push(block);
+        }
+        block.addLine(lineText, line.range, commentDelimiter);
+      } else {
+        block = null;
+      }
+    });
+
+    editor.edit((edit) => {
+      this.blocks.forEach((block) => {
+        block.updateEdit(edit, commentDelimiter);
+      });
+    });
+    this.blocks = [];
+  }
+}
+
+const formatter = new Formatter();
+export default formatter;
--- src/utils.ts
+++ src/utils.ts
@@ -9,3 +9,42 @@ export function setConfig(key: string, value: any) {
     .getConfiguration("comment-aligner")
     .update(key, value, true);
 }
+
+export function getCommentDelimiter(editor: vscode.TextEditor) {
+  switch (editor.document.languageId) {
+    case "javascript":
+      return "//";
+    case "typescript":
+      return "//";
+    case "javascriptreact":
+      return "//";
+    case "typescriptreact":
+      return "//";
+    case "html":
+      return "<!--";
+    case "xml":
+      return "<!--";
+    case "css":
+      return "/*";
+    case "scss":
+      return "/*";
+    case "less":
+      return "/*";
+    case "json":
+      return "//";
+    case "python":
+      return "#";
+    case "ruby":
+      return "#";
+    case "shellscript":
+      return "#";
+    case "powershell":
+      return "#";
+    case "properties":
+      return "#";
+    case "plaintext":
+      return "#";
+    default:
+      throw new Error("Unsupported language");
+  }
+}
